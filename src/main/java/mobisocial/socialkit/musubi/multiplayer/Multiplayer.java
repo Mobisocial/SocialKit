764106cd97db72c320677aada23f79f5ad59c6a7
Ben Dodson <bjdodson@gmail.com>
2011-10-04 22:33:08 -0700

    Enhancing multiplayer api; roundrobin for simple games, turnbased allows developer influence on state machine

--- src/main/java/mobisocial/socialkit/musubi/multiplayer/Multiplayer.java
+++ src/main/java/mobisocial/socialkit/musubi/multiplayer/Multiplayer.java
@@ -1,6 +1,12 @@
 package mobisocial.socialkit.musubi.multiplayer;
 
 public abstract class Multiplayer {
+    public static final String ACTION_TWO_PLAYERS = "mobisocial.intent.action.TWO_PLAYERS";
+    public static final String ACTION_MULTIPLAYER = "mobisocial.intent.action.MULTIPLAYER";
+
+    public static final String OBJ_MEMBERSHIP = "membership";
+    public static final String OBJ_MEMBER_CURSOR = "member_cursor";
+
     static final boolean DBG = true;
     static final String TAG = "musubi";
 }
