ed213e95ed15801a3e8c036be6e3bc4e16b1f7ec
Ben Dodson <bjdodson@gmail.com>
2011-10-17 15:21:05 -0700

    New CONNECTED launch intent

--- src/main/java/mobisocial/socialkit/musubi/multiplayer/Multiplayer.java
+++ src/main/java/mobisocial/socialkit/musubi/multiplayer/Multiplayer.java
@@ -16,8 +16,25 @@
 
 package mobisocial.socialkit.musubi.multiplayer;
 
+import mobisocial.socialkit.Feed;
+
 public abstract class Multiplayer {
+    /**
+     * Launch an application with a connection to a {@link Feed}.
+     * {@see Intent#CATEGORY_LAUNCHER}
+     */
+    public static final String ACTION_CONNECTED = "mobisocial.intent.action.CONNECTED";
+
+    /**
+     * Launch an application with a connection to a single remote user.
+     * {@see Intent#CATEGORY_LAUNCHER}
+     */
     public static final String ACTION_TWO_PLAYERS = "mobisocial.intent.action.TWO_PLAYERS";
+
+    /**
+     * Launch an application with a connection to remote users.
+     * {@see Intent#CATEGORY_LAUNCHER}
+     */
     public static final String ACTION_MULTIPLAYER = "mobisocial.intent.action.MULTIPLAYER";
 
     public static final String OBJ_MEMBERSHIP = "membership";
