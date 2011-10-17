56c8a23eb6f48da5d439e2b151c7d0c5c54bf108
Ben Dodson <bjdodson@gmail.com>
2011-10-17 11:53:19 -0700

    copyright headers

--- src/main/java/mobisocial/socialkit/SignedObj.java
+++ src/main/java/mobisocial/socialkit/SignedObj.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2011 The Stanford MobiSocial Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package mobisocial.socialkit;
 
 
 /**
- * An Obj that has been encoded and signed by a {@link User} to be shared with
- * a set of remote users with a common {@link Feed}.
+ * An Obj that has been encoded and signed by a {@link User} for sharing with
+ * a {@link Feed}.
  *
  */
 public interface SignedObj extends Obj {
